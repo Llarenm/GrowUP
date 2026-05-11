4ab624c420d8fab75ae404052fc9c195da0ebaf9
Llaren <llarenmorales@gmail.com>
2026-05-11 19:57:35 +0200

    actualización backend

--- src/main/java/com/growup/gestionestudiantes/repository/PersonaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.growup.gestionestudiantes.repository;
-
-import com.growup.gestionestudiantes.model.Persona;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonaRepository extends JpaRepository<Persona, Integer> {
-}
